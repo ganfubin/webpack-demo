39677a74d58219b86694d908a924a98b25defc57
ganfubin <ganfubin@zbj.com>
2018-09-05 13:05:38 +0800

    init
    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'webpack Vue.js'
+    }
+  },
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+
+  h1 {
+    color: green;
+  }
+}
+</style>
