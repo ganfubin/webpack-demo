85d21d2166d9714dcf86a59a8685e5a81ee22d0d
ganfubin <ganfubin@zbj.com>
2018-12-26 19:15:36 +0800

    node-webpack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -5,74 +5,74 @@ const uglifyjs = require('uglifyjs-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-    mode: 'development',
-   entry: ['babel-polyfill', './src/main.js'],
-    output: {
-        path: path.resolve(__dirname, './dist'), 
-        filename: '[name].js'  
+  mode: 'development',
+  entry: ['babel-polyfill', './src/main.js'],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js'
 
-    },
-    plugins: [
-    	new htmlWebpackPlugin({
-    		filename: 'index.html',
-    		template: './index.html',
-    		inject: true
-    	}),
-    	new VueLoaderPlugin(),
-        new uglifyjs(),
-    ],
-    resolve: {
-        alias: {
-            'vue': 'vue/dist/vue.js'
-        }
-    },
-    module: {
-        rules: [
-	        {
-			    test: /\.js$/,
-			    exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                      presets: ['@babel/preset-env'],
-                    }
-              }
-			},
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ],
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader',
-                    'less-loader'
-                ],
-            },
-            {
-			    test: /\.(png|jpg|gif|svg)$/,
-			    loader: 'file-loader',
-			    options: {
-			        name: '[name].[ext]?[hash]'
-			    }
-			},
-			{
-			    test: /\.vue$/,
-			    loader: 'vue-loader',
-			    options: {
-			        loaders: {
-			            'less': [
-			                'vue-style-loader',
-			                'css-loader',
-			                'less-loader?indentedSyntax'
-			            ]
-			        }
-			    }
-			}
-        ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      inject: true
+    }),
+    new VueLoaderPlugin(),
+    new uglifyjs(),
+  ],
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.js'
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'less-loader'
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'less': [
+              'vue-style-loader',
+              'css-loader',
+              'less-loader?indentedSyntax'
+            ]
+          }
+        }
+      }
+    ]
+  }
 };
